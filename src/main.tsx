22102f8b78e7de3cae4ddc00584f561a7828ffc2
nilsreich <nilsreich@gmail.com>
2023-11-30 16:41:00 +0000

    removed

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./globals.css";
 // eslint-disable-next-line
-import { registerSW } from "virtual:pwa-register"; // eslint-disable-line
-
-// add this to prompt for a refresh
-const updateSW = registerSW({
-  onNeedRefresh() {
-    if (confirm("New content available. Reload?")) {
-      updateSW(true);
-    }
-  },
-});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
